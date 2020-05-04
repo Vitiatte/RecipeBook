b94553af7f25f21b58c6e23bf9d655374b2b6e8a
vpavliuk <vpavliuk@varteq.com>
2020-05-04 20:00:46 +0300

    Set up project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Selection} from './header/selection'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'course-project';
+  selectedTabIndex = Selection.shopingList;
+
+  getSelection() {
+    return Selection;
+  }
 }
